78360bab221928100561fd23e51dfa968636d60d
harshnishad <harsh.nishad_cs21@gla.ac.in>
2025-01-21 12:47:48 +0530

    fixed bugs for deployment

--- src/Component/firebase.js
+++ src/Component/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBoudJvil0kFgvaKpy0EW9i3wmrXvzkKb8",
   authDomain: "assigment-23a2f.firebaseapp.com",
@@ -13,12 +10,13 @@ const firebaseConfig = {
   storageBucket: "assigment-23a2f.firebasestorage.app",
   messagingSenderId: "964048749871",
   appId: "1:964048749871:web:274691eaac9947bd0f0dda",
-  
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth=getAuth();
-export const db=getFirestore(app);
+// Export Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
 export default app;
